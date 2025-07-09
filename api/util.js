b75dd094733e0655c396da6be5ab0ea5f13d6071
Jaša Gaber <gaberjasa@gmail.com>
2025-07-09 08:31:40 +0200

    Visual Improvement

--- api/util.js
+++ api/util.js
@@ -1,4 +1,39 @@
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
-export { prisma };
+
+async function jwtMiddleware(req, res, next) {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        res.status(401).json({
+            "Error": "Missing token."
+        });
+    }
+
+    try {
+        const decodedToken = jwtMiddleware.verify(token, process.env.JWT_SECRET);
+        const existingUser = await prisma.user.findUnique({
+            where: { id: decodedToken.userId }
+        });
+
+        if (!user) {
+            return res.status(4004).json({
+                "Error": "User not found."
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(401).json({ "Error": "Invalid token." })
+    }
+
+}
+
+function cleanupUser(user) {
+    const { password, ...rest } = user;
+    return rest;
+}
+
+export { prisma, jwtMiddleware, cleanupUser };
